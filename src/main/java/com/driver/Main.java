3e96de26c1978dd526aaddf084233c00bf2faffe
mani <mani>
2022-12-31 09:30:44 +0530

    Version 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,14 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args)throws Exception {
 
-        BankAccount Mani=new BankAccount("Mani",5000,0);
-        CurrentAccount Gani=new CurrentAccount("Gani",10000,"BAT");
-        SavingsAccount mani=new SavingsAccount("mani",1000,3,5);
-        System.out.println(mani.getBalance());
-        mani.withdraw(500);
-        System.out.println(mani.getBalance());
-        System.out.println(mani.getSimpleInterest(2));
-        System.out.println(mani.getCompoundInterest(2,2));
+
 
     }
 }
